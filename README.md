0e5884672cd5838c542a5fa324a9dfd7f4c4bef4
Yuriy Jurayev <yuriy.jurayev.d@gmail.com>
2020-08-03 15:00:54 +0200

    feat: Adds a new TODO LISTS functionality, minor fixes and improvements
    Change log:
    	* app.py - Adds TODO LIST controller for CREATE, UPDATE, DELETE requests
    	* app.py - Improves complete TODO item functionality, considers corner cases
    	* index.js - Adds FE logic for creating a new list, completion and deletion
    	* index.html - Adds a new layout, css styles, and elements for TODO Category(LISTS) rendering

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO APP
+Full-Stack TODO application. Add your todo task, save as a specific category, and enjoy. Built with Python3+, Flask, SQAlchemy, PostgresSQL, MVC, React
+
+## Run application
+`$ pip3 install -r requirements.txt`
+`$ FLASK_APP=app.py FLASK_DEBUG=true flask run`
+`$ python3 app.py`
+
+## License
--- static/index.js
+++ static/index.js
@@ -1,13 +1,16 @@
 function createTodo(e) {
     e.preventDefault();
-    const descInput = document.getElementById('description');
+    let url = window.location.href.split('/');
+    const list_id = Number(url[url.length - 1]);
+    const descInput = document.getElementById('todos-description');
     const description = descInput.value;
     // Clear user input on submit
     descInput.value = '';
-    fetch('/todos/create', {
+    fetch(`/lists/${list_id}/todos/create`, {
         method: 'POST',
         body: JSON.stringify({
-            'description': description
+            'description': description,
+            'list_id': list_id
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -32,7 +35,7 @@ function deleteTodo(e) {
     .then(response => response.json())
     .then(data => {
         console.log("SUCCESS:", data);
-        document.getElementById(e.target.dataset.id).remove();
+        document.querySelector(`[id="todos"] [id="${e.target.dataset.id}"]`).remove();
         document.getElementById("error").className = 'hidden';
     })
     .catch((error) => {
@@ -41,7 +44,7 @@ function deleteTodo(e) {
     })
 }
 
-function changeTodoState(e) {
+function completeTodo(e) {
     console.log('Event:', e);
     fetch(`/todos/${e.target.dataset.id}/mark-completed`, {
         method: 'POST',
@@ -55,6 +58,12 @@ function changeTodoState(e) {
     .then(response => response.json())
     .then(data => {
         console.log("SUCCESS:", data);
+        const checkbox = document.querySelector(`[id='lists'] [id='${data.list_id}'] input`);
+        if (data.are_all_completed == true) {
+            checkbox.checked = true;
+        } else {
+            checkbox.checked = false;
+        }
         document.getElementById("error").className = 'hidden';
     })
     .catch((error) => {
@@ -67,27 +76,135 @@ function changeTodoState(e) {
 function renderNewTodo(data) {
     // Add a new to do record
     let li = document.createElement('li');
-    li.setAttribute('id', data.id);
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('id', 'delete');
-    deleteBtn.setAttribute('data-id', data.id);
-    deleteBtn.innerHTML += '&cross;'
-    li.appendChild(deleteBtn);
+    li.id = data.id;
 
     let checkbox = document.createElement('input');
-    checkbox.setAttribute('class', 'check');
+    checkbox.className = 'check';
     checkbox.setAttribute('data-id', data.id);
-    checkbox.setAttribute('type', 'checkbox');
+    checkbox.type = 'checkbox';
     li.appendChild(checkbox);
+    
+    let deleteBtn = document.createElement('button');
+    deleteBtn.id = 'todo-delete';
+    deleteBtn.setAttribute('data-id', data.id);
+    deleteBtn.innerHTML += '&cross;';
+    li.appendChild(deleteBtn);
 
     li.innerHTML += data.description;
     document.getElementById("todos").appendChild(li);
     // Add event listeners
-    document.querySelector(`li[id='${data.id}'] input`).addEventListener('change', (e) => changeTodoState(e));
+    document.querySelector(`li[id='${data.id}'] input`).addEventListener('change', (e) => completeTodo(e));
     document.querySelector(`li[id='${data.id}'] button`).addEventListener('click', (e) => deleteTodo(e));
     // Hide errors
     document.getElementById("error").className = 'hidden';
 }
 
-document.getElementById('form').addEventListener('submit', (e) => createTodo(e));
+function createList(e) {
+    e.preventDefault();
+    const input = document.getElementById('list-description');
+    const name = input.value;
+    // Clear user input on submit
+    input.value = '';
+    fetch('/lists/create', {
+        method: 'POST',
+        body: JSON.stringify({
+            'name': name
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("SUCCESS:", data);
+            renderNewList(data);
+        })
+        .catch((error) => {
+            console.log("ERROR:", error);
+            // Show errors
+            document.getElementById('error').className = '';
+        })
+
+}
+
+function deleteList(e) {
+    console.log('Event:', e)
+    fetch(`/lists/${e.target.dataset.id}/delete`, {method: 'DELETE'})
+    .then(response => response.json())
+    .then(data => {
+        console.log("SUCCESS:", data);
+        document.querySelector(`[id="lists"] [id="${e.target.dataset.id}"]`).remove();
+        document.getElementById("error").className = 'hidden';
+        window.location = "/";
+    })
+    .catch((error) => {
+        console.log("ERROR:", error);
+        document.getElementById("error").className = '';
+    })
+}
+
+function completeList(e) {
+    console.log('Event:', e);
+    const list_id = e.target.dataset.id;
+    fetch(`/lists/${list_id}/mark-completed`, {
+        method: 'POST',
+        body: JSON.stringify({
+            'completed': e.target.checked
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("SUCCESS:", data);
+        // SET/UNSET all todos
+        if (window.location.href.includes(`lists/${list_id}`)) {
+            for (let todo_id of data.todos) {
+                const checkbox = document.querySelector(`ul[id='todos'] li[id='${todo_id}'] input`);
+                if (data.completed == true) {
+                    checkbox.checked = true;
+                } else {
+                    checkbox.checked = false;
+                }
+            }
+        }
+        document.getElementById("error").className = 'hidden';
+    })
+    .catch((error) => {
+        console.log("ERROR:", error);
+        document.getElementById('error').className = '';
+    })
+
+}
+
+function renderNewList(data) {
+    // Add a new to do record
+    let li = document.createElement('li');
+    li.id = data.id;
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.id = 'list-delete';
+    deleteBtn.setAttribute('data-id', data.id);
+    deleteBtn.innerHTML += '&cross;';
+    li.appendChild(deleteBtn);
+
+    let checkbox = document.createElement('input');
+    checkbox.className = 'check';
+    checkbox.type = 'checkbox';
+    checkbox.setAttribute('data-id', data.id);
+    li.appendChild(checkbox);
+
+    li.innerHTML += `<a href="/lists/${data.id}">  ${data.name}  </a>`;
+    document.getElementById("lists").appendChild(li);
+    // Add event listeners
+    document.querySelector(`ul[id='lists'] li[id='${data.id}'] input`).addEventListener('change', (e) => completeList(e));
+    document.querySelector(`ul[id='lists'] li[id='${data.id}'] button`).addEventListener('click', (e) => deleteList(e));
+    // Hide errors
+    document.getElementById("error").className = 'hidden';
+    // Redirect to the new list
+    window.location = `/lists/${data.id}`;
+}
+
+document.getElementById('todos-form').addEventListener('submit', (e) => createTodo(e));
+document.getElementById('lists-form').addEventListener('submit', (e) => createList(e));
